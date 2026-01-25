a66ea49a531e40f72a9f067ba4deb51ede65b30a
hungg261 <phanthanhhung.26012010@gmail.com>
2026-01-25 23:23:59 +0700

    add Boruvka's MST

--- library/Graph/MST/boruvka.cpp
+++ library/Graph/MST/boruvka.cpp
@@ -1,17 +1,99 @@
 /**
  * Algorithm: Boruvka's Algorithm (MST)
+ * Checker(s):
+    - https://www.spoj.com/problems/MST
  * Notes:
     - Graph assumed to be connected
  */
-#include<bits/stdc++.h>
-using namespace std;
+struct DSU{
+    int n;
+    vector<int> par, sz;
 
+    DSU() = default;
+    DSU(int _sz): n(_sz){
+        par.resize(n + 1);
+        sz.resize(n + 1);
+    }
 
+    void init(){
+        for(int u = 0; u <= n; ++u)
+            create(u);
+    }
 
-signed main(){
-    ios_base::sync_with_stdio(0); cin.tie(0);
+    void create(int v){
+        par[v] = v;
+        sz[v] = 1;
+    }
 
+    int find_set(int v){
+        return par[v] == v ? v : par[v] = find_set(par[v]);
+    }
 
+    bool same(int u, int v){
+        return find_set(u) == find_set(v);
+    }
 
-    return 0;
+    bool union_sets(int a, int b){
+        a = find_set(a);
+        b = find_set(b);
+
+        if(a != b){
+            if(sz[a] < sz[b]) swap(a, b);
+
+            par[b] = a;
+            sz[a] += sz[b];
+
+            return true;
+        }
+
+        return false;
+    }
+};
+
+const int MAXN = 1e5;
+int n, m;
+struct Edge{
+    int u, v, w;
+
+    bool null(){
+        return u == -1;
+    }
+};
+vector<Edge> edges;
+int best_edges[MAXN + 5];
+
+long long Boruvka(){
+    int components = n;
+    DSU dsu(n);
+    dsu.init();
+
+    long long mst = 0;
+    while(components > 1){
+        for(int c = 1; c <= n; ++c)
+            best_edges[c] = -1;
+
+        for(int i = 0; i < m; ++i){
+            const Edge& e = edges[i];
+            int cu = dsu.find_set(e.u),
+                cv = dsu.find_set(e.v);
+            if(cu == cv) continue;
+
+            if(best_edges[cu] == -1 || e.w < edges[best_edges[cu]].w) best_edges[cu] = i;
+            if(best_edges[cv] == -1 || e.w < edges[best_edges[cv]].w) best_edges[cv] = i;
+        }
+
+        for(int c = 1; c <= n; ++c){
+            if(dsu.find_set(c) != c) continue;
+
+            const Edge& e = edges[best_edges[c]];
+            if(!dsu.same(e.u, e.v)){
+                dsu.union_sets(e.u, e.v);
+                mst += e.w;
+
+                --components;
+            }
+        }
+    }
+
+    return mst;
 }
